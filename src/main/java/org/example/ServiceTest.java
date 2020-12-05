0fd27c6e6734856a6f0d31340b9e68db5766e75a
艺术就是派大星 <65204073+251719824@users.noreply.github.com>
2020-12-05 22:56:02 +0800

    本次创建了一个ServiceTest 类 并添加一个打印方法

---
+++ src/main/java/org/example/ServiceTest.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: ServiceTest
+ * Author:   Administrator
+ * Date:     2020/12/5 0005 22:52
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package org.example;
+
+/**
+ * ClassName ServiceTest
+ * @Author Administrator
+ * @create 2020/12/5 0005 22:52
+ * @since 1.0.0
+ */
+public class ServiceTest {
+    public static void main(String[] args) {
+        print();
+    }
+    public  static  void print(){
+        System.out.println("今晚打老虎");
+    }
+
+}
